9c081191f0ea3cb9f37ca4473cd5c66453f5ff99
Andrew Owens <andrrowens@gmail.com>
2023-02-01 20:49:14 -0700

    add campsite and review controllers

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,2 +1,33 @@
 class CampsitesController < ApplicationController
+
+    def index 
+        render json: Campsite.all, status: :ok
+    end
+
+    def show 
+        campsite = Campsite.find_by(id:params[:id])
+        if campsite
+            render json: campsite, status: :ok
+        else 
+            render json: {error: "campsite not found"}, status: :not_found
+        end
+    end
+
+    def create
+        campsite = Campsite.create(campsite_params)
+        render json: campsite, status: :created    
+    end
+
+    def destroy
+        campsite = Campsite.find(params[:id])
+        campsite.destroy 
+        head :no_content 
+    end
+
+    private 
+
+    def campsite_params 
+        params.permit(:name, :region, :description, :elevation, :coordinates)
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,44 @@
 class ReviewsController < ApplicationController
+
+    def index 
+        render json: Review.all, status: :ok
+    end
+
+    def show 
+        review = Review.find_by(id:params[:id])
+        if review
+            render json: review, status: :ok
+        else 
+            render json: {error: "review not found"}, status: :not_found
+        end
+    end
+
+    def create
+        review = Review.create(review_params)
+        render json: review, status: :created    
+    end
+
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy 
+        head :no_content 
+    end
+
+    private 
+
+    def review_params 
+        params.permit(:campsite, :title, :date, :content, :image)
+    end
+
+    # review
+# t.string "campsite"
+# t.string "title"
+# t.integer "date"
+# t.string "content"
+# t.string "image"
+# t.integer "campsite_id"
+# t.integer "user_id"
+
+end
+
 end
