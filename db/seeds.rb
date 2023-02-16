5038d7a7a3b90df1c804b55896c868c34f6985ed
Andrew Owens <andrrowens@gmail.com>
2023-02-15 22:31:43 -0700

    more review css

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,16 +39,16 @@ c10 = Campsite.create(name: "Lost Lake", region: "San Juans", description: "Best
 # t.integer "coordinates"
 # t.integer "campsite_id"
 
-r1 = Review.create(title: "Windy but fun!", date: 060222 , content:"Beautiful but super windy!", image: "test", campsite_id: 1, user_id: 1)
-r2 = Review.create(title: "My Trip", date: 102222, content: "What a great trip.", image: "", campsite_id: 10, user_id: 5)
-r3 = Review.create(title: "October Backpacking", date: 100922, content: "It was such a good time.", image: "", campsite_id: 8, user_id: 3)
-r4 = Review.create(title: "Too many mountain goats", date: 053122, content: "Wayyyyy too many mountain goats.", image: "", campsite_id: 2, user_id: 4)
-r5 = Review.create(title: "Best Time Ever!", date: 061322, content: "Can't wait to go back to my favorite spot.", image: "", campsite_id: 6, user_id: 2)
-r6 = Review.create(title: "Rainy but fun", date: 101822, content: "Would have been the perfect trip if not for the rain", image: "", campsite_id: 3, user_id: 1)
-r7 = Review.create(title: "Want to go back", date: 072122, content: "I'm ready to go back tomorrow.", image: "", campsite_id: 9, user_id: 2)
-r8 = Review.create(title: "Alone in the woods", date: 061122, content: "Loved being away from it all.", image: "", campsite_id: 7, user_id: 4)
-r9 = Review.create(title: "Never Returning", date: 070422, content: "Three words - mud, bugs, and rain.", image: "", campsite_id: 4, user_id: 5)
-r10 = Review.create(title: "Met a bear...", date: 071722, content: "Met a bear and feeling lucky to be alive!", image: "", campsite_id: 5, user_id: 3)
+r1 = Review.create(title: "Windy but fun!", date: 060222, content:"Beautiful but super windy!", image: "https://freecampsites.net/wp-content/uploads/2018/06/20180618_101550-800x450.jpg", campsite_id: 1, user_id: 1)
+r2 = Review.create(title: "My Trip", date: 102222, content: "What a great trip.", image: "https://freecampsites.net/wp-content/uploads/2017/11/4031D4EC-12B9-4D32-BA1B-8EA3831E137B-450x600.jpeg", campsite_id: 10, user_id: 5)
+r3 = Review.create(title: "October Backpacking", date: 100922, content: "It was such a good time.", image: "https://freecampsites.net/wp-content/uploads/2020/07/6B34865E-081E-4CFC-8D12-7832D8551A8A-800x600.jpeg", campsite_id: 8, user_id: 3)
+r4 = Review.create(title: "Too many mountain goats", date: 053122, content: "Wayyyyy too many mountain goats.", image: "https://freecampsites.net/wp-content/uploads/2018/07/964D4C70-B0DF-42A2-9F81-C10713FF5DAF-800x600.jpeg", campsite_id: 2, user_id: 4)
+r5 = Review.create(title: "Best Time Ever!", date: 061322, content: "Can't wait to go back to my favorite spot.", image: "https://freecampsites.net/wp-content/uploads/2020/07/5E0FA740-8D18-463B-B942-5D61560D664C-800x600.jpeg", campsite_id: 6, user_id: 2)
+r6 = Review.create(title: "Rainy but fun", date: 101822, content: "Would have been the perfect trip if not for the rain", image: "https://freecampsites.net/wp-content/uploads/2020/09/IMG_20200914_202529-450x600.jpg", campsite_id: 3, user_id: 1)
+r7 = Review.create(title: "Want to go back", date: 072122, content: "I'm ready to go back tomorrow.", image: "https://freecampsites.net/wp-content/uploads/2019/10/20190928_115407-800x600.jpg", campsite_id: 9, user_id: 2)
+r8 = Review.create(title: "Alone in the woods", date: 061122, content: "Loved being away from it all.", image: "https://freecampsites.net/wp-content/uploads/2019/06/IMG_20190622_075218-800x600.jpg", campsite_id: 7, user_id: 4)
+r9 = Review.create(title: "Never Returning", date: 070422, content: "Three words - mud, bugs, and rain.", image: "https://freecampsites.net/wp-content/uploads/2016/08/Screen-Shot-2016-08-24-at-1.08.25-PM.png", campsite_id: 4, user_id: 5)
+r10 = Review.create(title: "Met a bear...", date: 071722, content: "Met a bear and feeling lucky to be alive!", image: "https://freecampsites.net/wp-content/uploads/2018/07/IMG_7448-800x600.jpg", campsite_id: 5, user_id: 3)
 
 # review
 # t.string "campsite"
